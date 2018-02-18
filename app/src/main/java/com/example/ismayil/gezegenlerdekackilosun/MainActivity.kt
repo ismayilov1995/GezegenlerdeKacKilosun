0e6bf2bf627716d38456109287d02fea5e1dfa90
ismayilov1995 <ismayilov1995@users.noreply.github.com>
2018-02-19 03:22:47 +0400

    Initial commit

---
+++ app/src/main/java/com/example/ismayil/gezegenlerdekackilosun/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.example.ismayil.gezegenlerdekackilosun
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val kG_TO_POUND = 2.20462
+    private val pound_to_kg = 0.453592
+    val planetArray = listOf(0.38,0.91,0.38,2.34,1.06,0.92,1.19)
+    val planetNameArray = listOf("Mercury","Venus","Mars","Jupiter","Saturn","Uranus","Neptune")
+    var arrayPos = 0
+    var userWeightInPound :Double = 0.0
+    var weightInPlanetPound :Double = 0.0
+    var weightInPlanetKG :Double = 0.0
+    var userWeightInKg :Editable?= null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        userWeightInKg = editText.text
+
+        val mercury = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.mercury)
+        val venus = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.venus)
+        val mars = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.mars)
+        val jupiter = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.jupiter)
+        val saturn = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.saturn)
+        val uranus = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.uranus)
+        val neptune = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.neptune)
+
+        val planetImageArray = ArrayList<Bitmap>()
+        planetImageArray.add(mercury)
+        planetImageArray.add(venus)
+        planetImageArray.add(mars)
+        planetImageArray.add(jupiter)
+        planetImageArray.add(saturn)
+        planetImageArray.add(uranus)
+        planetImageArray.add(neptune)
+
+        //Hocam ben burada range 0..7 kullaninca hata veriyordu nedenini anlayamadim
+
+        ivNext.setOnClickListener {
+            if (arrayPos in 0..5){
+                arrayPos++
+                imageView.setImageBitmap(planetImageArray[arrayPos])
+                tVPlanet.text = planetNameArray[arrayPos]
+                avtomatikHesabla()
+            }
+        }
+
+        //Ve burada 7..1 e yazmak zorunda kaldim bunu da deneme yanilma yontemiyle yaptim. nedenini anlayamadim
+        ivBack.setOnClickListener {
+            if (arrayPos in 7 downTo 1){
+                arrayPos--
+                imageView.setImageBitmap(planetImageArray[arrayPos])
+                tVPlanet.text = planetNameArray[arrayPos]
+                avtomatikHesabla()
+            }
+        }
+
+
+
+        hesabla.setOnClickListener {
+            secilenPlanetiHesabla()
+        }
+
+    }
+
+    fun avtomatikHesabla(){
+        if (checkBox.isChecked){
+            secilenPlanetiHesabla()
+        }else{
+            //nothing
+        }
+    }
+
+    fun secilenPlanetiHesabla(){
+        userWeightInPound = kgToPound(userWeightInKg.toString().toDouble())
+        weightInPlanetPound = userWeightInPound*planetArray[arrayPos]
+        weightInPlanetKG = poundToKg(weightInPlanetPound)
+        tvResult.text = weightInPlanetKG.formatla(2).toString()
+    }
+
+
+    fun kgToPound(kg : Double):Double{
+        return kg*kG_TO_POUND
+    }
+
+    fun poundToKg(pound:Double):Double{
+        return pound*pound_to_kg
+    }
+
+    fun Double.formatla(kacTaneRakam:Int)=java.lang.String.format("%.${kacTaneRakam}f",this)
+}
+
