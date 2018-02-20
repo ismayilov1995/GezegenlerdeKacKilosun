1c7c2d148168423298a695a16b4a78b9b84f78e7
ismayilov1995 <ismayilov1995@users.noreply.github.com>
2018-02-20 17:50:09 +0400

    kicik deyishiklikler

--- app/src/main/java/com/example/ismayil/gezegenlerdekackilosun/MainActivity.kt
+++ app/src/main/java/com/example/ismayil/gezegenlerdekackilosun/MainActivity.kt
@@ -8,18 +8,19 @@ import android.text.Editable
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private val kG_TO_POUND = 2.20462
     private val pound_to_kg = 0.453592
-    val planetArray = listOf(0.38,0.91,0.38,2.34,1.06,0.92,1.19)
-    val planetNameArray = listOf("Mercury","Venus","Mars","Jupiter","Saturn","Uranus","Neptune")
+    val planetArray = listOf(0.38, 0.91, 0.38, 2.34, 1.06, 0.92, 1.19)
+    val planetNameArray = listOf("Mercury", "Venus", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
     var arrayPos = 0
-    var userWeightInPound :Double = 0.0
-    var weightInPlanetPound :Double = 0.0
-    var weightInPlanetKG :Double = 0.0
-    var userWeightInKg :Editable?= null
+    var userWeightInPound: Double = 0.0
+    var weightInPlanetPound: Double = 0.0
+    var weightInPlanetKG: Double = 0.0
+    var userWeightInKg: Editable? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,13 +30,13 @@ class MainActivity : AppCompatActivity() {
 
         userWeightInKg = editText.text
 
-        val mercury = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.mercury)
-        val venus = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.venus)
-        val mars = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.mars)
-        val jupiter = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.jupiter)
-        val saturn = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.saturn)
-        val uranus = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.uranus)
-        val neptune = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.neptune)
+        val mercury = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.mercury)
+        val venus = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.venus)
+        val mars = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.mars)
+        val jupiter = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.jupiter)
+        val saturn = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.saturn)
+        val uranus = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.uranus)
+        val neptune = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.neptune)
 
         val planetImageArray = ArrayList<Bitmap>()
         planetImageArray.add(mercury)
@@ -46,27 +47,29 @@ class MainActivity : AppCompatActivity() {
         planetImageArray.add(uranus)
         planetImageArray.add(neptune)
 
+
+
         //Hocam ben burada range 0..7 kullaninca hata veriyordu nedenini anlayamadim
 
         ivNext.setOnClickListener {
-            if (arrayPos in 0..5 && !userWeightInKg.isNullOrEmpty()){
+            if (arrayPos in 0..5 && !userWeightInKg.isNullOrEmpty()) {
                 arrayPos++
                 imageView.setImageBitmap(planetImageArray[arrayPos])
                 tVPlanet.text = planetNameArray[arrayPos]
                 avtomatikHesabla()
-            }else if (userWeightInKg.isNullOrEmpty()){
+            } else if (userWeightInKg.isNullOrEmpty()) {
                 ifEditTextIsEmpt()
             }
         }
 
         //Ve burada 7..1 e yazmak zorunda kaldim bunu da deneme yanilma yontemiyle yaptim. nedenini anlayamadim
         ivBack.setOnClickListener {
-            if (arrayPos in 7 downTo 1 && !userWeightInKg.isNullOrEmpty()){
+            if (arrayPos in 7 downTo 1 && !userWeightInKg.isNullOrEmpty()) {
                 arrayPos--
                 imageView.setImageBitmap(planetImageArray[arrayPos])
                 tVPlanet.text = planetNameArray[arrayPos]
                 avtomatikHesabla()
-            }else if (userWeightInKg.isNullOrEmpty()){
+            } else if (userWeightInKg.isNullOrEmpty()) {
                 ifEditTextIsEmpt()
             }
         }
@@ -75,48 +78,48 @@ class MainActivity : AppCompatActivity() {
 
         hesabla.setOnClickListener {
             if (!userWeightInKg.isNullOrEmpty())
-            secilenPlanetiHesabla()
+                secilenPlanetiHesabla()
             else ifEditTextIsEmpt()
         }
 
         checkBox.setOnClickListener {
-            if(checkBox.isChecked){
+            if (checkBox.isChecked) {
                 hesabla.visibility = View.INVISIBLE
-            }else{
+            } else {
                 hesabla.visibility = View.VISIBLE
             }
         }
 
     }
 
-    fun avtomatikHesabla(){
-        if (checkBox.isChecked){
+    fun avtomatikHesabla() {
+        if (checkBox.isChecked) {
             secilenPlanetiHesabla()
-        }else{
+        } else {
             //nothing
         }
     }
 
-    fun ifEditTextIsEmpt(){
-        Toast.makeText(this,"YUXARIYA KILONUZU GIRIN",Toast.LENGTH_SHORT).show()
+    fun ifEditTextIsEmpt() {
+        Toast.makeText(this, "YUXARIYA KILONUZU GIRIN", Toast.LENGTH_SHORT).show()
     }
 
-    fun secilenPlanetiHesabla(){
+    fun secilenPlanetiHesabla() {
         userWeightInPound = kgToPound(userWeightInKg.toString().toDouble())
-        weightInPlanetPound = userWeightInPound*planetArray[arrayPos]
+        weightInPlanetPound = userWeightInPound * planetArray[arrayPos]
         weightInPlanetKG = poundToKg(weightInPlanetPound)
         tvResult.text = weightInPlanetKG.formatla(2).toString()
     }
 
 
-    fun kgToPound(kg : Double):Double{
-        return kg*kG_TO_POUND
+    fun kgToPound(kg: Double): Double {
+        return kg * kG_TO_POUND
     }
 
-    fun poundToKg(pound:Double):Double{
-        return pound*pound_to_kg
+    fun poundToKg(pound: Double): Double {
+        return pound * pound_to_kg
     }
 
-    fun Double.formatla(kacTaneRakam:Int)=java.lang.String.format("%.${kacTaneRakam}f",this)
+    fun Double.formatla(kacTaneRakam: Int) = java.lang.String.format("%.${kacTaneRakam}f", this)
 }
 
